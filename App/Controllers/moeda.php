ee84fa8c6af8a73f43d969423c289d6586df8fee
César Ferreira <cesarferreiraa67@gmail.com>
2021-02-11 18:49:08 -0300

    concluido

--- App/Controllers/moeda.php
+++ App/Controllers/moeda.php
@@ -15,48 +15,11 @@ class Moeda extends Controller{
         $this->viewDash('moedas/cotacoesGerais', $dados = ['gerais' => $dados]);
     }
 
-    public function favoritas(){
+    public function especificas(){
 
-    	Acesso::checkLogin();
-
-        $this->viewDash('moedas/cotacoesFavoritas');
-    }
-
-    public function listaAdicionarFav(){
-
-    	Acesso::checkLogin();
-
-    	$moedas = $this->model('MoedasDao');
-        //$dados = $moedas->listaAdicionarMoedasFavoritas();
-        $dados = $moedas->listaOpcoesMoedas();
-
-        //Carrega moedas já adicionadas pelo usuário
-
-        $moedasFav = $moedas->buscaFavoritasUsuario($_SESSION['userId']);
-
-        $this->viewDash('moedas/listaAddFav', $dados = ['listAddFav' => $dados, 'moedasFavoritas' => $moedasFav]);
-    }
-
-    public function adicionarFav($moeda){
-
-    	Acesso::checkLogin();
-
-    	$mensagem = array();
-
-    	$MoedasDao = $this->model('MoedasDao');
-
-    	$buscaIdMoeda = $MoedasDao->buscarMoedaEspecifica($moeda);
-
-    	$moeda = $this->model('Moedas');
-    	$moeda->setUsuario($_SESSION['userId']);
-    	$moeda->setMoedaId($buscaIdMoeda);
-
-    	$mensagem[] = $MoedasDao->adicionarMoedaFavorita($moeda);
     	
-    	//Carregar informações das moedas
-
-        $dados = $MoedasDao->listaAdicionarMoedasFavoritas();
-
-    	$this->viewDash('moedas/listaAddFav', $dados = ['listAddFav' => $dados, 'mensagem' => $mensagem]);
     }
+
+    
+
 }
